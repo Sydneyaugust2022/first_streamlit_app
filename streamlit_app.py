31f66e8e41addf413baaad7f366a7b0dc779ed49
Sydneyaugust2022 <111260432+Sydneyaugust2022@users.noreply.github.com>
2022-08-18 10:33:38 +1000

    Update streamlit_app.py
    adding more lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 
-streamlit.title('My Parenst New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 
 
 streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.title('Kale, Spinach & Rocket Smoothie')
+streamlit.title('Hard-Boiled Free-Range Egg')
