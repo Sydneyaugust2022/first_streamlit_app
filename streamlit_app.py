791dad6cefec27c27f4d816543b1b2ecf49caa56
Sydneyaugust2022 <111260432+Sydneyaugust2022@users.noreply.github.com>
2022-08-18 10:37:13 +1000

    Update streamlit_app.py
    no reference to streamlit.footer

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,3 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-
-
-streamlit.footer('End of Menu')
