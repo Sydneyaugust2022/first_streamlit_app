8c446c8917022ca9c89a53bb61d381f4e4efa4e7
Sydneyaugust2022 <111260432+Sydneyaugust2022@users.noreply.github.com>
2022-08-22 09:23:30 +1000

    Update streamlit_app.py
    adding smoothie title

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.title('My test albert')
+streamlit.title('Build Your Own Fruit Smoothie')
