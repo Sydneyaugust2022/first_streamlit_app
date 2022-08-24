295396e4a50668f017c8e6a9a4c75f437a3dadff
Sydneyaugust2022 <111260432+Sydneyaugust2022@users.noreply.github.com>
2022-08-24 10:14:37 +1000

    Update streamlit_app.py
    adding a section to display fruityvice response

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # This line to show selection of fruits from the table
 streamlit.dataframe(fruits_to_show)
+
+# This section to display fruityvice response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
