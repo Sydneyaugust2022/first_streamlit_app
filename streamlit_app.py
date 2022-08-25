d984426c71dc0e4cdc1727ef493c8e90820beb46
Sydneyaugust2022 <111260432+Sydneyaugust2022@users.noreply.github.com>
2022-08-25 10:39:03 +1000

    Update streamlit_app.py
    adding line for user to input jackfruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,3 +65,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("chapter 12 from the training material adding snowflake connector")
 #streamlit.text("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+#
+###### this is from exercise lesson 12 streamlit challenge ####
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
