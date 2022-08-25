401211caa123919cd9a0e6906d397167105c22c3
Sydneyaugust2022 <111260432+Sydneyaugust2022@users.noreply.github.com>
2022-08-25 10:29:02 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,11 +56,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 # This section to display snowflake connector
-streamlit.header("chapter 12 from the training material adding snowflake connector")
+#streamlit.header("chapter 12 from the training material adding snowflake connector")
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
+streamlit.header("chapter 12 from the training material adding snowflake connector")
+#streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
