928cb3ab73facde3aeac27136d8fd399dfc51daf
Sydneyaugust2022 <111260432+Sydneyaugust2022@users.noreply.github.com>
2022-08-25 10:16:58 +1000

    Update streamlit_app.py
    lesson 12

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,3 +58,9 @@ streamlit.dataframe(fruityvice_normalized)
 # This section to display snowflake connector
 streamlit.header("chapter 12 from the training material adding snowflake connector")
 import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
